d48852bc1120edb0f74cc83f95a85558f3e9e661
Guilherme Rodrigues <Brodrigues.batista2002@gmail.com>
2021-07-07 01:30:57 -0300

    Projeto icatalogo

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    require("../vendor/autoload.php");
+
+    $app = new \App\Core\Router();
+
+    // $produtoModel = new \App\Model\Produto();
+
+    // print_r(explode("/", "mvc.icatalogo.com.br/produtos/index"));
+
+    // echo "<br>";
+
+    // $lista = $produtoModel->listarTodos();
+
+    // foreach($lista as $produto){
+    //     echo $produto->descricao;
+    // }
